766b302ea388a8292782123de1519d31d5dbbc7d
Flogo345 <Flogo345@web.de>
2019-11-11 11:41:43 +0100

    last cahnges again

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public GameObject VrCam;
+    public GameObject FpCam;
+    public int CamMode;
+    private const int _numberCamModes = 2;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Camera"))
+        {
+            CamMode = (CamMode++) %_numberCamModes;
+            StartCoroutine(CamChange());
+        }
+        
+    }
+
+    IEnumerator CamChange()
+    {
+        yield return new WaitForSeconds(0.01f);
+        switch (CamMode)
+        {
+            case 0:
+                VrCam.SetActive(true);
+                FpCam.SetActive(false);
+                break;
+            case 1:
+                FpCam.SetActive(true);
+                VrCam.SetActive(false);
+                break;
+        };
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    //define Scenes here
+    public enum Scene
+    {
+        SampleScene,
+    }
+
+    //call this to load new scene (at the moment sync -> lag/freeze)
+    public static void Load(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+    public static void Load(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
