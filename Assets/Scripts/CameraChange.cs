37fcbbd54f375ec6c3cd6dc294fd73bd8a020143
trho96 <tran_hai@gmx.de>
2019-11-11 12:27:45 +0100

    changes added to main computer

--- Assets/Scripts/CameraChange.cs
+++ Assets/Scripts/CameraChange.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class CameraChange : MonoBehaviour
 {
     public GameObject VrCam;
+    public GameObject VrObj;
     public GameObject FpCam;
     public int CamMode;
     private const int _numberCamModes = 2;
+
+    void Start()
+    {
+        VrCam.SetActive(false);
+        VrObj.SetActive(false);
+        FpCam.SetActive(true);
+    }
 
     void Update()
     {
@@ -26,10 +34,12 @@ public class CameraChange : MonoBehaviour
         {
             case 0:
                 VrCam.SetActive(true);
+                VrObj.SetActive(true);
                 FpCam.SetActive(false);
                 break;
             case 1:
                 FpCam.SetActive(true);
+                VrObj.SetActive(false);
                 VrCam.SetActive(false);
                 break;
         };
